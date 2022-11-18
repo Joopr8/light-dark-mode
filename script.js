745bf6d36f71bec15d73a5575a8206de4908b671
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-18 18:46:12 +0000

    Code review, clean the code

--- script.js
+++ script.js
@@ -5,33 +5,40 @@ const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
+const DARK_THEME = "dark";
+const LIGHT_THEME = "light";
 
 const switchTheme = (e) => {
   if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    localStorage.setItem("theme", "dark");
-    toogleDarkLightMode(false);
+    document.documentElement.setAttribute("data-theme", DARK_THEME);
+    localStorage.setItem("theme", DARK_THEME);
+    toogleDarkLightMode(DARK_THEME);
   } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "light");
-    toogleDarkLightMode(true);
+    document.documentElement.setAttribute("data-theme", LIGHT_THEME);
+    localStorage.setItem("theme", LIGHT_THEME);
+    toogleDarkLightMode(LIGHT_THEME);
   }
 };
 
-function toogleDarkLightMode(isLight) {
-  nav.style.backgroundColor = isLight
-    ? "rgb(255 255 255 / 50%)"
-    : "rgb(0 0 0 / 50%)";
-  textBox.style.backgroundColor = isLight
-    ? "rgb(0 0 0 / 50%)"
-    : "rgb(255 255 255 / 50%)";
-  toogleIcon.children[0].textContent = isLight ? "Light Mode" : "Dark Mode";
-  isLight
-    ? toogleIcon.children[1].classList.replace("fa-moon", "fa-sun")
-    : toogleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  isLight ? changeImgSource(image1, "light") : changeImgSource(image1, "dark");
-  isLight ? changeImgSource(image2, "light") : changeImgSource(image2, "dark");
-  isLight ? changeImgSource(image3, "light") : changeImgSource(image3, "dark");
+function toogleDarkLightMode(mode) {
+  nav.style.backgroundColor =
+    mode === LIGHT_THEME ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor =
+    mode === LIGHT_THEME ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+  toogleIcon.children[0].textContent =
+    mode === LIGHT_THEME ? "Light Mode" : "Dark Mode";
+  mode === LIGHT_THEME
+    ? toogleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toogleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  mode === LIGHT_THEME
+    ? changeImgSource(image1, LIGHT_THEME)
+    : changeImgSource(image1, DARK_THEME);
+  mode === LIGHT_THEME
+    ? changeImgSource(image2, LIGHT_THEME)
+    : changeImgSource(image2, DARK_THEME);
+  mode === LIGHT_THEME
+    ? changeImgSource(image3, LIGHT_THEME)
+    : changeImgSource(image3, DARK_THEME);
 }
 
 const changeImgSource = (element, color) => {
@@ -45,9 +52,8 @@ const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
-
-  if (currentTheme === "dark") {
-    toogleDarkLightMode(false);
+  if (currentTheme === DARK_THEME) {
+    toogleDarkLightMode(DARK_THEME);
     toogleSwitch.checked = true;
   }
 }
